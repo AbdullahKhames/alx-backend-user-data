44101f8da9c2fa15b3ca37c567d75a833985f0a4
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-14 23:47:25 +0200

    boiler plate

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+module to check auth
+""" 
+from typing import TypeVar
+from flask import request
+
+
+class Auth:
+    """class to represent authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method for check require auth
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            bool: returns False - 
+            path and excluded_paths will be used later,
+            now, you don’t need to take care of them
+        """
+        pass
+
+
+    def authorization_header(self, request=None) -> str:
+        """method to add header
+
+        Args:
+            request (_type_, optional): flask req obj
+
+        Returns:
+            str: None - request will be the Flask request object
+        """
+        pass
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user
+        returns:
+            returns None - request will be the Flask request object
+        """
