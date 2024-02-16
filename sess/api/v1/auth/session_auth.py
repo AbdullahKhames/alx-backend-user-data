d59288e100c873290074fef83fb75373d0ac3401
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-16 22:55:17 +0200

    new

--- sess/api/v1/auth/session_auth.py
+++ sess/api/v1/auth/session_auth.py
@@ -1,5 +1,8 @@
+from os import getenv
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+
+from models.user import User
 
 class SessionAuth(Auth):
     user_id_by_session_id = {}
@@ -16,3 +19,9 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
                     return None 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """(overload) that returns a User instance based on a cookie value:"""
+        if  request is None:
+            return None
+        return User.get(self.user_id_for_session_id(request.cookies.get(getenv('SESSION_NAME'))))
