e6f2bcb15b36a23a68c4d28fe32967b71c6bb626
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-16 23:10:01 +0200

    new

--- sess/api/v1/auth/session_auth.py
+++ sess/api/v1/auth/session_auth.py
@@ -1,7 +1,8 @@
 from os import getenv
+from flask import request, jsonify
 from api.v1.auth.auth import Auth
 from uuid import uuid4
-
+from api.v1.views import app_views
 from models.user import User
 
 class SessionAuth(Auth):
@@ -25,3 +26,23 @@ class SessionAuth(Auth):
         if  request is None:
             return None
         return User.get(self.user_id_for_session_id(request.cookies.get(getenv('SESSION_NAME'))))
+
+
+@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    if email is None or email == '':
+        return jsonify({ "error": "email missing" }), 400
+    password  = request.form.get('password ')
+    if password  is None or password  == '':
+        return jsonify({ "error": "password missing" }), 400
+    user = User.search({'email': email})
+    if user is None:
+        return jsonify({ "error": "no user found for this email" }), 404
+    if not user.is_valid_password(password):
+        return jsonify({ "error": "wrong password" }), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    out = jsonify(user.to_json(True))
+    out.set_cookie(getenv('SESSION_NAME'), session_id)
+    return out
