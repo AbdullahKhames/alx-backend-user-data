12c11cd097171f99965cccbd7ae7e7e479057ff5
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 16:14:21 +0200

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 """module for implementing user module functionalities"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def bienvenue():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False):
+def users():
+    """method to register users"""
+    print(request.form)
 
 
 if __name__ == "__main__":
