f461d936fdfb793e5bd904af0612465d32458b0b
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 16:06:17 +0200

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 """module for implementing user module functionalities"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slash=False)
+def bienvenue():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
