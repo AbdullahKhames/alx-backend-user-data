332f63ea931fd035587f6d7499cbdc2bfec69522
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 14:57:21 +0200

    find user by arbitrary arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -31,10 +32,28 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        method to add user to db
+        """ Adds user to database
+        Return: User Object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """
+        method to find by arbitrary args
+        """
+        # user_attrs = ['id', 'email', 'hashed_password','session_id', 'reset_token']
+        my_filters = set()
+
+        for k, v in kwargs.items():
+            if hasattr(User, k):
+                my_filters.add(getattr(User, k) == v)
+            else:
+                raise InvalidRequestError
+        user = self._session.query(User).filter(and_(*my_filters)).first()
+        if user is None:
+            raise NoResultFound
         return user
