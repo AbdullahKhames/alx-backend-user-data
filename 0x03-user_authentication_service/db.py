38cd5b98d399e21269c76a0c18bb1de1227346dc
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 15:13:48 +0200

    find user by arbitrary arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine, and_
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -17,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,30 +33,20 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to database
-        Return: User Object
+        """ adds user to database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-
         return user
 
-    def find_user_by(self, **kwargs: dict) -> User:
-        """
-        method to find by arbitrary args
-        """
-        if not kwargs:
-            raise InvalidRequestError
-
-        columns = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in columns:
+    def find_user_by(self, **kwargs) -> User:
+        """ find user function """
+        users = self._session.query(User)
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
                 raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-
-        if user is None:
-            raise NoResultFound
-
-        return user
+            for user in users:
+                if getattr(user, key) == value:
+                    return user
+                raise NoResultFound
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""module for implementing user module functionalities"""
+""" declarative base"""
 
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    """user model definition"""
-    __tablename__ = 'users'
+    """ User model """
+
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
