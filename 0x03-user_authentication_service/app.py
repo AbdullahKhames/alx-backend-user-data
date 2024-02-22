3ce5bb7e018c4ece00e61c00118432f249d374e7
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 16:46:03 +0200

    session saved in db and loggs in user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """module for implementing user module functionalities"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 from user import User
 
@@ -23,6 +23,20 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """method to login users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        sess_id = AUTH.create_session(email)
+        out = jsonify(state=0, msg={"email": email, "message": "logged in"})
+        out.set_cookie('session_id', sess_id)
+        return out
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
