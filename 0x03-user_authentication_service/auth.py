070b6c96a01e371592832a7d7122f3c74fd69814
AbdullahKhames <abdullahkhames96@gmail.com>
2024-02-22 15:29:25 +0200

    hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-"""module for implementing user module functionalities"""
+"""module for implementing auth module functionalities"""
+from bcrypt import gensalt, hashpw
+
+
+def _hash_password(password: str) -> bytes:
+    """method that takes in a password and returns its hash"""
+    return hashpw(password.encode('utf-8'), gensalt())
